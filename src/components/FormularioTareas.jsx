187571ade513196f7af28e00aa6fbf0d6392076e
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-09-26 10:14:54 -0300

    ägregamos el value en el input y limpiamos el formulario

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,23 +1,37 @@
-import React from 'react';
-import {Form, Button} from 'react-bootstrap'
-import ListTareas from './ListTareas';
-const FormularioTareas = () => {
-    return (
-        <div>
-             <Form>
-      <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-        
-        <Form.Control type="text" placeholder="Ingrese una Tarea" />
-        <Button variant="primary" type="submit">
-        Enviar
-      </Button>
-      </Form.Group>
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import ListTareas from "./ListTareas";
 
-      
-    </Form>
-    <ListTareas></ListTareas>
-        </div>
-    );
+const FormularioTareas = () => {
+  const [tarea, setTarea] = useState("");
+  const [arregloTarea, setArregloTarea] = useState([]);
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setArregloTarea([...arregloTarea, tarea]);
+    setTarea('')
+
+    
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una Tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value = {tarea}
+          />
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form.Group>
+      </Form>
+      <ListTareas></ListTareas>
+    </div>
+  );
 };
 
 export default FormularioTareas;
